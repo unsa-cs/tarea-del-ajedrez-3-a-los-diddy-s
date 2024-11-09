9c29eeece063f7d4998efe0927dc858425cec9f1
ElQuesoHelado <josealvacornejo1@gmail.com>
2024-11-08 22:31:29 -0500

    Se va a utilizar la funcion flipV definida en chess.h para las pruebas, se crea una figura 1x1(la mas basica) para analizar el unlink de memoria

--- chess.c
+++ chess.c
@@ -1,36 +1,38 @@
-//#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
 #include "chess.h"
 #include "gc.h"
+#include <stdlib.h>
 
-char** allocateMemory(int rows, size_t cols){
-  char** newFig;
-  memoryAlloc((void**)&newFig, sizeof(char*)*(rows + 1));
-  for(int i = 0; i < rows; i++)
-    memoryAlloc((void**)&newFig[i], sizeof(char)*(cols + 1));
+char **allocateMemory(int rows, size_t cols) {
+  char **newFig;
+  memoryAlloc((void **)&newFig, sizeof(char *) * (rows + 1));
+  for (int i = 0; i < rows; i++)
+    memoryAlloc((void **)&newFig[i], sizeof(char) * (cols + 1));
   return newFig;
 }
 
-void unlinkMemory(char** fig){
+void unlinkMemory(char **fig) {
   countMemoryEntries();
-  for(int i = 0; fig[i]; i++)
-    unregisterPointer((void**)&fig[i]);
+  for (int i = 0; fig[i]; i++)
+    unregisterPointer((void **)&fig[i]);
   countMemoryEntries();
-  unregisterPointer((void**)&fig);
+  unregisterPointer((void **)&fig);
   countMemoryEntries();
 }
 
-char** reverse(char** fig){
+char **reverse(char **fig) {
   int rows = 0;
-  while(fig[++rows]);
-  
+  while (fig[++rows])
+    ;
+
   int cols = 0;
-  while(fig[0][++cols]);
+  while (fig[0][++cols])
+    ;
 
-  char** newFig = allocateMemory(rows, cols);
+  char **newFig = allocateMemory(rows, cols);
 
-  for(int i = 0; fig[i]; i++){
-    for(int j = 0; fig[0][j]; j++)
+  for (int i = 0; fig[i]; i++) {
+    for (int j = 0; fig[0][j]; j++)
       newFig[i][j] = fig[i][j];
     newFig[i][cols] = 0;
   }
@@ -39,3 +41,10 @@ char** reverse(char** fig){
   return newFig;
 }
 
+char **flipV(char **fig) {
+  fig = allocateMemory(1, 1);
+
+  unlinkMemory(fig);
+
+  return fig;
+}
